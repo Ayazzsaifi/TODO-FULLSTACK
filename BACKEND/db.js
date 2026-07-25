513913dc11542fd9be93a0c8ca6b8a7584f873fd
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-07-25 17:07:09 +0530

    backend: auth + todo CRUD with JWT and MongoDB

---
+++ BACKEND/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.ObjectId;
+
+const user = new Schema({
+    name: String,
+    email: {type:String,unique:true},
+    password: String
+});
+
+const todo = new Schema({
+    title: String,
+    done: {type:Boolean, default: false},
+    userId: ObjectId
+})
+
+const userModel = mongoose.model('users', user);
+const todoModel = mongoose.model('todo', todo);
+
+module.exports = {
+    userModel: userModel,
+    todoModel: todoModel
+}
---
+++ BACKEND/index.js
@@ -0,0 +1,145 @@
+require('dotenv').config()
+const bcrypt = require("bcrypt")
+const express = require("express");
+const app = express();
+const { userModel, todoModel } = require("./db");
+const jwt = require("jsonwebtoken");
+const { default: mongoose } = require("mongoose");
+const JWT_SECRET = process.env.JWT_SECRET
+app.use(express.json());
+// will add a zod library in this 
+const {z}=require("zod");
+
+
+mongoose.connect(process.env.MONGO_URI)
+
+app.post("/signup", async function (req, res) {
+
+    //input validation
+
+    const requireBody=z.object({
+        email: z.string().min(3).max(30).email(),
+        password: z.string().min(3).max(20).regex(/[A-Z]/).regex(/[a-z]/).regex(/[0-9]/).regex(/[^A-Za-z0-9]/),
+        name: z.string().min(3).max(20)
+    })
+
+    const parsedDataWithSuccess = requireBody.safeParse(req.body);
+
+
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message:"wrong format",
+            error:parsedDataWithSuccess.error
+        })
+        return
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    // hasing password
+
+    try {
+        const hassedPassword = await bcrypt.hash(password, 5);
+        console.log(hassedPassword);
+
+        await userModel.create({
+            email: email,
+            password: hassedPassword,
+            name: name
+        })
+    }
+    catch(e){
+        res.json({
+            message:"Email already Exists"
+        })
+    }
+
+
+    res.json({
+        message: "youre Sign up "
+    })
+
+});
+
+app.post("/signin", async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await userModel.findOne({
+        email: email
+    });
+
+    if (!user) {
+        res.status(404).json({
+            message: "this user is not in our DataBase"
+        })
+        return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+
+    console.log(user);
+
+    if (passwordMatch) {
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_SECRET);
+        res.json({
+            token: token
+        });
+    }
+
+    else {
+        res.status(403).json({
+            error: "incorrect credintaials"
+        })
+    }
+});
+
+app.post("/todo", auth, async function (req, res) {
+    const userId = req.userId;
+    const title = req.body.title;
+
+    const newTodo = await todoModel.create({
+        title,
+        userId
+    })
+
+    res.json({
+        message: "Todo Added"
+    })
+
+});
+
+app.get("/todos", auth,async function (req, res) {
+    const userId = req.userId; 
+
+    const todos = await todoModel.find({
+        userId: userId
+    })
+
+    res.json({
+        todos
+    })
+
+});
+
+
+function auth(req, res, next) {
+    const token = req.headers.token
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if (decodedData) {
+        req.userId = decodedData.id;
+        next();
+    }
+    else {
+        res.status(403).json({
+            err: "incorrect credintials"
+        })
+    }
+}
+
+app.listen(3000);
