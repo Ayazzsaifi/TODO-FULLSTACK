07bffdb25dd77aabe51c46bd90e2214c048d9932
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-07-25 23:56:10 +0530

    frontend: signup flow complete + auth working end to end

--- FRONTEND/script.js
+++ FRONTEND/script.js
@@ -2,6 +2,7 @@ const loginBtn =document.getElementById('login-btn');
 const signUpbtn=document.getElementById('signup-btn');
 const emailInput=document.getElementById('email-input');
 const passwordInput=document.getElementById('password-input');
+const nameInput= document.getElementById("name-input");
 
 
 loginBtn.addEventListener("click",async function(e){
@@ -12,3 +13,15 @@ loginBtn.addEventListener("click",async function(e){
     localStorage.setItem( "token",response.data.token)
     window.location.href="todo.html"
 });
+
+signUpbtn.addEventListener("click",async function(e){
+    e.preventDefault()
+    const email=emailInput.value;
+    const password=passwordInput.value
+    const name=nameInput.value
+    const response=await axios.post("http://localhost:3000/signup",{email,password,name})
+    alert("Signup successful! Please login.")
+    emailInput.value="";
+    passwordInput.value="";
+    nameInput.value=""
+})
